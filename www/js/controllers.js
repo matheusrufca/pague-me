956fec04e28ba1ec808b2707c3bd4d15b9758d2b
Matheus Rufca <matheus.rufca@gmail.com>
2016-10-18 16:15:32 -0200

    Added get friends service

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,84 @@
 angular.module('starter.controllers', ['starter.services', 'ngOpenFB'])
 
-.controller('DashCtrl', function ($scope) { })
+.controller('DashCtrl', function ($scope, $state, facebookAuthService, facebookService) {
+	var self = {};
+
+	self.init = function () {
+		facebookAuthService.getLoginStatus().then(function (response) {
+			if (response.status === 'connected') {
+				$scope.getFriends(); // get friends
+			} else {
+				$state.go('tab.login'); // the user isn't logged in to Facebook. go to login state
+			}
+		});
+	};
+
+	$scope.getFriends = function () {
+		facebookService.getFriends().then(function (response) {
+			console.debug(JSON.stringify(response));
+		});
+	};
+
+
+	self.init();
+})
+
+
+.controller('AccountCtrl', function ($scope, $state, facebookAuthService) {
+	$scope.settings = {
+		enableFriends: true
+	};
+
+
+	$scope.logout = function () {
+		facebookAuthService.logout().then(function () {
+			$state.go('tab.login');
+		});
+	};
+})
+
+.controller('LoginController', function ($scope, $state, $ionicModal, $timeout, facebookAuthService) {
+	self.init = function () {
+		facebookAuthService.getLoginStatus().then(function (response) {
+			if (response.status === 'connected') {
+				$state.go('tab.dash');
+			}
+		});
+	};
+
+	//This method is executed when the user press the "Login with facebook" button
+	$scope.facebookSignIn = function () {
+		var callback = function (response) {
+			if (response.status === 'connected') {
+				console.log('Facebook login succeeded');
+				$state.go('tab.profile'); // go do dashboard
+			} else {
+				console.log('Facebook login failed');
+				alert('Facebook login failed');
+			};
+		}
+
+		facebookAuthService.login().then(callback); //login on facebook
+	};
+
+
+
+	self.init();
+})
+
+.controller('ProfileController', function ($scope, ngFB) {
+	ngFB.api({
+		path: '/me',
+		params: { fields: 'id,name' }
+	}).then(
+        function (user) {
+        	$scope.user = user;
+        },
+        function (error) {
+        	alert('Facebook error: ' + error.error_description);
+        });
+})
+
 
 .controller('ChatsCtrl', function ($scope, Chats) {
 	// With the new view caching in Ionic, Controllers are only called
@@ -19,46 +97,4 @@ angular.module('starter.controllers', ['starter.services', 'ngOpenFB'])
 
 .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
 	$scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function ($scope, $state, ngFB) {
-	$scope.settings = {
-		enableFriends: true
-	};
-
-
-	$scope.logout = function () {
-		ngFB.logout().then(function () {
-			$state.go('tab.login');
-		});
-	};
-})
-
-.controller('LoginController', function ($scope, $state, $ionicModal, $timeout, ngFB) {
-	//This method is executed when the user press the "Login with facebook" button
-	$scope.facebookSignIn = function () {
-		ngFB.login().then(
-        function (response) {
-        	if (response.status === 'connected') {
-        		console.log('Facebook login succeeded');
-        		//$scope.closeLogin();
-        		$state.go('tab.profile');
-        	} else {
-        		alert('Facebook login failed');
-        	}
-        });
-	};
-})
-
-.controller('ProfileController', function ($scope, ngFB) {
-	ngFB.api({
-		path: '/me',
-		params: { fields: 'id,name' }
-	}).then(
-        function (user) {
-        	$scope.user = user;
-        },
-        function (error) {
-        	alert('Facebook error: ' + error.error_description);
-        });
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,95 @@
-angular.module('starter.services', [])
+angular.module('starter.services', ['ngOpenFB'])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+.factory('Chats', function () {
+	// Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+	// Some fake testing data
+	var chats = [{
+		id: 0,
+		name: 'Ben Sparrow',
+		lastText: 'You on your way?',
+		face: 'img/ben.png'
+	}, {
+		id: 1,
+		name: 'Max Lynx',
+		lastText: 'Hey, it\'s me',
+		face: 'img/max.png'
+	}, {
+		id: 2,
+		name: 'Adam Bradleyson',
+		lastText: 'I should buy a boat',
+		face: 'img/adam.jpg'
+	}, {
+		id: 3,
+		name: 'Perry Governor',
+		lastText: 'Look at my mukluks!',
+		face: 'img/perry.png'
+	}, {
+		id: 4,
+		name: 'Mike Harrington',
+		lastText: 'This is wicked good ice cream.',
+		face: 'img/mike.png'
+	}];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
-  };
-});
+	return {
+		all: function () {
+			return chats;
+		},
+		remove: function (chat) {
+			chats.splice(chats.indexOf(chat), 1);
+		},
+		get: function (chatId) {
+			for (var i = 0; i < chats.length; i++) {
+				if (chats[i].id === parseInt(chatId)) {
+					return chats[i];
+				}
+			}
+			return null;
+		}
+	};
+})
+
+
+.service('facebookAuthService', ['ngFB', function (ngFB) {
+	var self = this;
+
+	self.login = function () {
+		return ngFB.login({ scope: 'user_friends' })
+	};
+
+	self.logout = function () {
+		return ngFB.logout();
+	};
+
+	self.getLoginStatus = function () {
+		return ngFB.getLoginStatus(function (response) {
+			if (response.status === 'connected') {
+				// the user is logged in and has authenticated your
+				// app, and response.authResponse supplies
+				// the user's ID, a valid access token, a signed
+				// request, and the time the access token 
+				// and signed request each expire
+				var uid = response.authResponse.userID;
+				var accessToken = response.authResponse.accessToken;
+			} else if (response.status === 'not_authorized') {
+				// the user is logged in to Facebook, 
+				// but has not authenticated your app
+			} else {
+				// the user isn't logged in to Facebook.
+			}
+		});
+	};
+}])
+
+
+
+.service('facebookService', ['ngFB', function (ngFB) {
+	var self = this;
+
+	self.getFriends = function () {
+		return ngFB.api({
+			path: '/me',
+			params: { fields: 'friends' }
+		});
+	};
+}]);
